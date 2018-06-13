7a71cd8f2d7934f48c2f2ed00f8fd67f34852929
Ilya SIBAN <i.siban@talentoday.com>
2018-06-13 12:24:40 +0200

    Updated app.js

--- app.js
+++ app.js
@@ -3,26 +3,31 @@
 
   var express        = require("express"),
       livereload     = require("connect-livereload"),
+      bodyParser     = require("body-parser"),
       app            = express(),
-      server         = require("http").createServer(app);
+      server         = require("http").createServer(app),
+      common         = require("./common"),
+      config         = common.config();
 
-  var config = {
-    liverealoadPort: 35729,
-    port: 8080
-  };
 
   app.use(livereload({ port: config.livereloadPort}));
   app.use(express.static("./build"));
 
   app.set("views", __dirname + "/views");
 
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+
+
   // Route API
   var portfolios = require("./routes/portfolios"),
-      skills = require("./routes/skills");
+      skills = require("./routes/skills"),
+      contact = require("./routes/mailto");
 
 
   app.use("/api/portfolios", portfolios);
   app.use("/api/skills", skills);
+  app.use("/api/contact", contact);
 
   app.get("*", function (req, res) {
     res.sendFile(__dirname + "/build/index.html");
